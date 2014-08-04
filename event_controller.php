b44509d71580517641e71f1f150eaf28937db333
XaroRSA <Kenneth@fmp.za.net>
2014-08-04 21:37:04 +0200

    removed lines

--- event_controller.php
+++ event_controller.php
@@ -38,18 +38,16 @@
       $priority = get('priority');
       $message = get('message');
 
-<<<<<<< HEAD
 	  //Twilio added XaroRSA
 	  $fromNumber = get('fromNumber');
 	  $toNumber = get('toNumber');
 	  $premute = get('premute');
 	  
       $event->add($userid,$eventfeed,$eventtype,$eventvalue,$action,$setfeed,$setemail,$setvalue,$callcurl,$message,$mutetime,$priority,$fromNumber,$toNumber,$premute);
-=======
-      $event->add($userid,$eventfeed,$eventtype,$eventvalue,$action,$setfeed,$setemail,$setvalue,$callcurl,$message,$mutetime,$priority);
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
+
       $result = "Event added";
     }
+	
     if ($route->action == 'edit' && $session['write'])
     {
       $eventid = intval(get('eventid'));
@@ -64,7 +62,6 @@
       $mutetime = get('mutetime');
       $priority = get('priority');
       $message = get('message');
-<<<<<<< HEAD
 	  
 	  //Twilio added XaroRSA
 	  $fromNumber = get('fromNumber');
@@ -72,10 +69,6 @@
 	  $premute = get('premute');
 
       $event->update($userid,$eventid,$eventfeed,$eventtype,$eventvalue,$action,$setfeed,$setemail,$setvalue,$callcurl,$message,$mutetime,$priority,$fromNumber,$toNumber,$premute);
-=======
-
-      $event->update($userid,$eventid,$eventfeed,$eventtype,$eventvalue,$action,$setfeed,$setemail,$setvalue,$callcurl,$message,$mutetime,$priority);
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
       $result = "Event updated";
     }
 
@@ -130,17 +123,12 @@
       $consumersecret = post('consumersecret');
       $usertoken = post('usertoken');
       $usersecret = post('usersecret');
-<<<<<<< HEAD
 	  
 	  //Added XaroRSA
 	  $sid = post('sid');
 	  $token = post('token');
 
       $result = $event->set_settings($session['userid'],$prowlkey,$consumerkey,$consumersecret,$usertoken,$usersecret,$smtpserver,$smtpuser,$smtppassword,$smtpport,$nmakey, $sid, $token);
-=======
-
-      $result = $event->set_settings($session['userid'],$prowlkey,$consumerkey,$consumersecret,$usertoken,$usersecret,$smtpserver,$smtpuser,$smtppassword,$smtpport,$nmakey);
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
     }
 
     else if ($session['write'])
--- event_settings_view.php
+++ event_settings_view.php
@@ -6,11 +6,7 @@
 <form action="savesettings" method="post">
 
 <div class="row-fluid">
-<<<<<<< HEAD
   <div class="span3">
-=======
-  <div class="span4">
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
 
     <h3><?php echo _('Email settings'); ?></h3>
     
@@ -32,21 +28,14 @@
 
     <select name="smtpport">
         <option <?php if ($settings['smtpport'] == '25') { echo "selected"; }?>  value="25">25 (No SSL)</option>
-<<<<<<< HEAD
 		<option <?php if ($settings['smtpport'] == '26') { echo "selected"; }?>  value="26">26 (No SSL)</option>
-=======
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
         <option <?php if ($settings['smtpport'] == '465') { echo "selected"; }?> value="465">465 (SSL)</option>    
     </select>
     <br>
 
   </div>
 
-<<<<<<< HEAD
   <div class="span3">
-=======
-  <div class="span4">
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
 
     <h3><?php echo _('Twitter settings'); ?></h3>
     <label><?php echo _('Consumer key:'); ?></label>
@@ -60,30 +49,23 @@
     <br>
     
   </div>
-<<<<<<< HEAD
+
   <div class="span3">
-=======
-  <div class="span4">
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
 
     <h3><?php echo _('Prowl settings'); ?></h3>
     <label><?php echo _('Prowl key:'); ?></label>
     <input type="text" name="prowlkey" value="<?php echo $settings['prowlkey']; ?>" />
     <br>
-
-<<<<<<< HEAD
-=======
   </div>
+  
   <div class="span4">
 
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
     <h3><?php echo _('NMA settings'); ?></h3>
     <label><?php echo _('NMA key:'); ?></label>
     <input type="text" name="nmakey" value="<?php echo $settings['nmakey']; ?>" />
     <br>
-
   </div>
-<<<<<<< HEAD
+
   <div class="span3">
   <h3><?php echo _('Twilio settings'); ?></h3>
     <label><?php echo _('SID:'); ?></label>
@@ -94,10 +76,7 @@
     <input type="text" name="token" value="<?php echo $settings['token']; ?>" />
     <br>
   </div>
-  
-=======
 
->>>>>>> 186539af82a78e6cff844365618443bc1d95bec9
 </div>
 
     <input type="submit" class="btn btn-danger" value="<?php echo _('Change'); ?>" />
