cafbc8f578bc28c99fbdb4d96018743b1fea9ae5
XaroRSA <kenneth@fmp.za.net>
2014-08-12 15:47:56 +0200

    Fixed previous ?, indent fixed, will only display first time label if premute is enabled aka > 0

--- event_list.php
+++ event_list.php
@@ -67,8 +67,9 @@
     if ($item['action']!= 1)
     {
 		if ($item['premute']!=0){ 
-	  echo "<span class='label label-info' >First time: ".date("Y-n-j H:i:s", $item['firsttime'])."</span>";}
-      echo "<span class='label label-info' >Last true: ".date("Y-n-j H:i:s", $item['lasttime'])."</span>";
+			echo "<span class='label label-info' >First time: ".date("Y-n-j H:i:s", $item['firsttime'])."</span>";}
+			
+		echo "<span class='label label-info' >Last true: ".date("Y-n-j H:i:s", $item['lasttime'])."</span>";
     }
     ?></td>
 
