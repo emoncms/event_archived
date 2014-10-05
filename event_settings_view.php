7fa7fa0561963b8148c464bc1069eb56fadc4995
Ben Jones <ben.jones12@gmail.com>
2014-10-05 22:28:05 +1300

    Fixed MQTT password setting display

--- event_settings_view.php
+++ event_settings_view.php
@@ -78,7 +78,7 @@
     $salt = $user->get_salt($session['userid']);
     $mqttpassword   = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($settings['mqttpassword']), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
     ?>
-    <input type="text" name="mqttpassword" value="<?php echo $settings['mqttusername']; ?>" />
+    <input type="password" name="mqttpassword" value="<?php echo $mqttpassword; ?>" />
     <br>
   </div>
 
