ac7bdc15ce873a2067d3f00e219f9d484bc87956
Augustin Cisterne-Kaas <augustin.cisternekaas@elico-corp.com>
2015-10-14 23:38:03 +0800

    Added an icon to the event menu.

--- event_menu.php
+++ event_menu.php
@@ -1,3 +1,4 @@
 <?php
-
-  $menu_dropdown[] = array('name'=>"Event", 'path'=>"event" , 'session'=>"write", 'order' => 10 );
+$menu_dropdown[] = array(
+    'name'=> 'Event', 'path'=>'event', 'session'=>'write', 'order' => 10,
+    'icon' => 'icon-info-sign');
